e9f3b1aeca69b870f8fa83debcca952cc46bacb8
Krists Krievins <krists.krievins@gmail.com>
2025-04-24 12:47:13 +0200

    Completed lab

--- src/iteration2.ts
+++ src/iteration2.ts
@@ -2,4 +2,22 @@
 // Iteration 2 | Functions
 //
 
+const calcMultiplication = (a: number, b: number): number => {
+  return a * b;
+};
 
+const isEven = (n: number): boolean => {
+  return n % 2 === 0;
+};
+
+const calcArrayAverage = (numArray: number[]): number | string => {
+  if (numArray.length === 0) {
+    return "Please provide an array with at least one element";
+  } else {
+    const total = numArray.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue;
+    });
+    const average = total / numArray.length;
+    return average;
+  }
+};
--- src/iteration3.ts
+++ src/iteration3.ts
@@ -2,4 +2,35 @@
 // Iteration 3 | Classes
 //
 
+class bankAccount {
+  balance: number = 0;
+  accountHolder: string = "";
 
+  constructor(accountHolder: string) {
+    this.accountHolder = accountHolder;
+  }
+
+  getBalance(): number {
+    return this.balance;
+  }
+
+  deposit(amount: number): number | string {
+    if (amount <= 0) {
+      return "Please provide a valid amount";
+    } else {
+      this.balance += amount;
+      return this.balance;
+    }
+  }
+
+  withdraw(amount: number): number | string {
+    if (amount <= 0) {
+      return "Please provide a valid amount";
+    } else if (amount > this.balance) {
+      return "Insufficient funds";
+    } else {
+      this.balance -= amount;
+      return this.balance;
+    }
+  }
+}
